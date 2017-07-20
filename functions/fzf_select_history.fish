d70115b7104813e7bd90eaec1587772a229e2ca3
Hirofumi Wakasugi <baenej@gmail.com>
2017-07-20 19:15:03 +0900

    Do not sort the result from the history

--- functions/fzf_select_history.fish
+++ functions/fzf_select_history.fish
@@ -1,5 +1,5 @@
 function fzf_select_history -d "fzf source to select from history"
-    history | fzf | read entry
+    history | fzf --no-sort | read entry
 
     if test -n "$entry"
         commandline $entry
